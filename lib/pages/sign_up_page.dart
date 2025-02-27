d5b4f98b1c80c57cfabd387b9a0ce4a8a8c848aa
dnddsf3 <148702929+dnddsf3@users.noreply.github.com>
2025-02-27 10:50:03 +0700

    merged with main

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -1,18 +1,281 @@
+import 'package:booknest/main.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool _isPasswordObscured = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: backgroundColor,
-      ),
+      backgroundColor: lightColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home-page');
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
 
-      body: const Center(child: Text("This is Sign Up Page")),
+                      // First Name, Last Name, Email, Username, Password, and Password Confirmation
+                      _buildTextField('First Name', 'First Name'),
+                      _buildTextField('Last Name', 'Last Name'),
+                      _buildTextField('Email', 'Email'),
+                      _buildTextField('Username', 'Username'),
+                      _buildPasswordField('Password', 'Password'),
+                      _buildPasswordField('Password Confirmation', 'Confirm Password'),
+
+                      const SizedBox(height: 20),
+
+                      // Sign Up Button
+                      _buildSignUpButton(),
+
+                      const SizedBox(height: 20),
+
+                      // Divider with OR text in the center
+                      _buildDividerWithText("OR"),
+
+                      const SizedBox(height: 20),
+
+                      // Sign Up With Google Button
+                      _buildGoogleSignUpButton(),
+
+                      const SizedBox(height: 20),
+
+                      // Already have an account? Sign In
+                      Align(
+                        alignment: Alignment.center,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, "/sign-in");
+                          },
+                          child: Text(
+                            "Already have an account? Sign In",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  // Helper function to build text fields
+  Widget _buildTextField(String label, String hint) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 5),
+        TextField(
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            fillColor: Color(0xFFE8E8E8),
+            filled: true,
+            hintText: hint,
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+
+  // Helper function to build password fields
+  Widget _buildPasswordField(String label, String hint) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 5),
+        TextField(
+          obscureText: _isPasswordObscured,
+          decoration: InputDecoration(
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isPasswordObscured ? Icons.visibility_off : Icons.visibility,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isPasswordObscured = !_isPasswordObscured;
+                });
+              },
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            fillColor: Color(0xFFE8E8E8),
+            filled: true,
+            hintText: hint,
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+
+  // Helper function to build Sign Up Button
+  Widget _buildSignUpButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: ElevatedButton(
+          onPressed: () {
+            print("Sign Up ditekan");
+          },
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            backgroundColor: Color(0xFFC8C6BA),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: Text(
+            "Sign Up",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Helper function to build Divider with Text
+  Widget _buildDividerWithText(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Row(
+        children: [
+          Expanded(
+            child: Divider(
+              thickness: 1,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Divider(
+              thickness: 1,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Helper function to build Sign Up with Google Button
+  Widget _buildGoogleSignUpButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: ElevatedButton(
+          onPressed: () {
+            print("Sign Up With Google ditekan");
+          },
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            backgroundColor: Color(0xFFC8C6BA),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/google.png",
+                height: 20,
+                width: 20,
+              ),
+              const SizedBox(width: 16),
+              Text(
+                "Sign Up With Google",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
